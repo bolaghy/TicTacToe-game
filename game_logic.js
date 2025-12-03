90793e0aa3e7c4e232a60f9f14e1c9f3312adc62
bolaghy <bolajiisaac10@yahoo.com>
2025-12-03 08:13:20 +0100

    refresh

--- game_logic.js
+++ game_logic.js
@@ -1,109 +1,83 @@
-const playerText = document.getElementById('playerText')
-const restartBtn = document.getElementById('restartBtn')
-const boxes = Array.from(document.getElementsByClassName('box'))
+const playerText = document.getElementById("playerText");
+const restartBtn = document.getElementById("restartBtn");
+const boxes = Array.from(document.getElementsByClassName("box"));
 
-const winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+const winnerIndicator = getComputedStyle(document.body).getPropertyValue(
+  "--winning-blocks"
+);
 
-const O_TEXT = "O"
-const X_TEXT = "X"
-let currentPlayer = X_TEXT
-let spaces = Array(9).fill(null)
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
 let gameOver = false;
 
-
 const startGame = () => {
-    boxes.forEach(box => box.addEventListener('click', boxClicked))
-
-}
+  boxes.forEach((box) => box.addEventListener("click", boxClicked));
+};
 
 function boxClicked(e) {
-    const id = e.target.id
+  const id = e.target.id;
 
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer;
+    e.target.innerText = currentPlayer;
 
+    if (playerHasWon() !== false) {
+      playerText.innerHTML = `${currentPlayer} has won!`;
 
-    if (!spaces[id]) {
-        spaces[id] = currentPlayer
-        e.target.innerText = currentPlayer
-
-        if (playerHasWon() !== false) {
-            playerText.innerHTML = `${currentPlayer} has won!`
-
-            let winning_blocks = playerHasWon()
-            spaces = Array(9).fill(null); 
-            winning_blocks.map(box => boxes[box].style.backgroundColor = winnerIndicator)
-            gameOver = true;
-
-
-
-        }
-        if (spaces.every(space => space !== null) && !gameOver) {
-            playerText.innerHTML = 'No Winner!';
-            gameOver = true;
-        }
-
-
-
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+      let winning_blocks = playerHasWon();
+      spaces = Array(9).fill(null);
+      winning_blocks.map(
+        (box) => (boxes[box].style.backgroundColor = winnerIndicator)
+      );
+      gameOver = true;
+    }
+    if (spaces.every((space) => space !== null) && !gameOver) {
+      playerText.innerHTML = "No Winner!";
+      gameOver = true;
     }
 
+    currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+  }
 }
-
-
-
-
-
-
-
-
 
 const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function playerHasWon() {
-    for (const condition of winningCombos) {
-        let [a, b, c] = condition
+  for (const condition of winningCombos) {
+    let [a, b, c] = condition;
 
-        if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
-            return [a, b, c]
-        }
+    if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+      return [a, b, c];
     }
-    return false
+  }
+  return false;
 }
 
-
-
-
-
-restartBtn.addEventListener('click', restart)
-
+restartBtn.addEventListener("click", restart);
 
 function restart() {
-    spaces.fill(null)
+  spaces.fill(null);
 
-    boxes.forEach(box => {
-        box.innerText = ''
-        box.style.backgroundColor = ''
-    })
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.style.backgroundColor = "";
+  });
 
-    playerText.innerHTML = 'Tic Tac Toe'
+  playerText.innerHTML = "Tic Tac Toe";
 
-
-
-    currentPlayer = X_TEXT
-    gameOver = false;
-
-
+  currentPlayer = X_TEXT;
+  gameOver = false;
 }
 
-
-
-
-startGame()
+startGame();
